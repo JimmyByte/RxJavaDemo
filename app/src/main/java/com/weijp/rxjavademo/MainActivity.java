36395a362d5f406c7f1518f9f13e4eb9be2df7f6
weijunpeng <weijunpeng@hljia.cn>
2018-11-16 15:46:37 +0800

    Initial commit

---
+++ app/src/main/java/com/weijp/rxjavademo/MainActivity.java
@@ -0,0 +1,50 @@
+package com.weijp.rxjavademo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+                emitter.onNext(1);
+                emitter.onNext(2);
+                emitter.onNext(3);
+                emitter.onComplete();
+            }
+        }).subscribe(new Observer<Integer>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.e("rx-","开始");
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                Log.e("rx-",integer+"");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e("rx-","error");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.e("rx-","oncomplete");
+            }
+        });
+    }
+}
